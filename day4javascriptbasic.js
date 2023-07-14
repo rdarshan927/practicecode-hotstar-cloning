193d5d22a6dacd2347688dd88c9ecfbf132999e3
rdarshan927 <rdarshan927@gmail.com>
2023-07-14 21:09:07 +0530

    day 5 basic javascript

---
+++ day4javascriptbasic.js
@@ -0,0 +1,12 @@
+console.log("Hello World")
+
+console.log(1 + 2)
+
+var name = "Darshan R"
+
+console.log(name)
+
+console.log(typeof("Darshan R"))
+console.log(typeof(21))
+
+document.getElementById("test").innerHTML = "Hi";
