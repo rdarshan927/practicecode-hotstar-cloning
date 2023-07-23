763924c16683ff2232da43c2792e71c63195081f
rdarshan927 <rdarshan927@gmail.com>
2023-07-23 21:32:25 +0530

    top slider with js almost

---
+++ index.js
@@ -0,0 +1,44 @@
+let movies = [
+    {
+        name: "Loki",
+        description:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, consequatur incidunt eum in voluptatum ad odio cupiditate magnam quis cum deleniti modi maxime totam omnis, nam quae! Iste, tenetur officiis.",
+        image: ""
+    },
+    {
+        name: "Loki",
+        description:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, consequatur incidunt eum in voluptatum ad odio cupiditate magnam quis cum deleniti modi maxime totam omnis, nam quae! Iste, tenetur officiis.",
+        image: ""
+    },
+    {
+        name: "Loki",
+        description:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, consequatur incidunt eum in voluptatum ad odio cupiditate magnam quis cum deleniti modi maxime totam omnis, nam quae! Iste, tenetur officiis.",
+        image: ""
+    },
+    {
+        name: "Loki",
+        description:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, consequatur incidunt eum in voluptatum ad odio cupiditate magnam quis cum deleniti modi maxime totam omnis, nam quae! Iste, tenetur officiis.",
+        image: ""
+    },
+    {
+        name: "Loki",
+        description:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, consequatur incidunt eum in voluptatum ad odio cupiditate magnam quis cum deleniti modi maxime totam omnis, nam quae! Iste, tenetur officiis.",
+        image: ""
+    },
+    {
+        name: "Loki",
+        description:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, consequatur incidunt eum in voluptatum ad odio cupiditate magnam quis cum deleniti modi maxime totam omnis, nam quae! Iste, tenetur officiis.",
+        image: ""
+    },
+];
+
+const topcontainer=document.querySelector('.top-image-container');
+
+let sliders=[];
+
+let sliderIndex= 0;     //using to track the current slide.
